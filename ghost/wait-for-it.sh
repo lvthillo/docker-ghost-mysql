568508b573dd0754f2b078472ce7741cdae26dc8
lvthillo <lorenz.vanthillo@outlook.com>
2018-01-13 16:13:37 +0100

    remove nginx + update versions

--- ghost/wait-for-it.sh
+++ ghost/wait-for-it.sh
@@ -6,11 +6,10 @@ host="$1"
 shift
 cmd="$@"
 
-until mysql -h"$host" -p"3306" -u"blog-user" -p"supersecret" -D"db" ; do
+until mysql -hmysql -p"3306" -u"${database__connection__user}" -p"${database__connection__password}" -D"${database__connection__database}" ; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "MySQL is up - executing command"
 exec "$@"
-#exec ../../../entrypoint.sh npm start
